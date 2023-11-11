07f32e33ecf6e9f0e4e8ce2b0714febd37e51151
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-10 23:52:29 -0500

    Creating and placing new nodes

--- FoundationsCourse/JS/MDNTutorial/script.js
+++ FoundationsCourse/JS/MDNTutorial/script.js
@@ -3,3 +3,18 @@ const link = document.querySelector('a');
 link.textContent = "Mozilla DEVELOPER Network";
 link.href = "https://developer.mozilla.org";
 link.target = "_blank";
+
+const sect = document.querySelector('section');
+
+const para = document.createElement('p');
+para.textContent = 'We hope you enyoyed the ride.';
+sect.appendChild(para);
+
+
+const text = document.createTextNode(
+  " — the premier source for web development knowledge.",
+);
+
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
+
