f6ee57854ef8b5074ee9242097bb69cc7f16f132
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-11 01:01:54 -0500

    Agrega el comportamiento del boton Add item

---
+++ Area51/ZonaJS/Shopping-List/script.js
@@ -0,0 +1,20 @@
+const ul = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.onclick = () => {
+  let value = input.value;
+  input.value = "";
+  let li = document.createElement('li');
+  let span = document.createElement('span');
+  let btn = document.createElement('button');
+
+  li.appendChild(span);
+  li.appendChild(btn);
+
+  span.textContent = value;
+  btn.textContent = "Delete";
+
+  ul.appendChild(li);
+};
+
