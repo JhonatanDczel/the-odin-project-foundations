023a70f8eeda56a1e08e8f742ba528be7aa16264
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-14 20:19:36 -0500

    Tasks

--- FoundationsCourse/JS/array-methods/code.js
+++ FoundationsCourse/JS/array-methods/code.js
@@ -1,3 +1,11 @@
+let titleStyle = 'color: white; font-size: 24px; font-weight: bold; margin: 12px; border: 4px solid indigo; padding: 12px; border-radius: 10px;';
+let subTitleStyle = 'color: white; font-size: 16px;font-weight: bold; margin: 8px; margin: 0; padding: 0';
+function title(str) {
+  console.log(`%c${str}`, titleStyle);
+}
+function subTitle(str) {
+  console.log(`%c${str}`, subTitleStyle);
+}
 //We have methods for array like this:
 //array.pop();
 //array.push();
@@ -40,12 +48,57 @@ console.table(subArray);
 let countries = ['Peru', 'Colombia', 'Bolivia', 'China'];
 
 function alrt(item, index, array) {
-  alert(`${item} is at index ${index} in ${array}`);
+  console.log(`${item} is at index ${index} in ${array}`);
 }
 
 countries.forEach(alrt);
 
 
+//SEARCH methods
+title('SEARCH METHODS');
+
+arr = [1, 0, false];
+
+console.log(arr.indexOf(0));
+
+console.log(arr.indexOf(false));
+
+//Find methods
+//Finwindex
+//findLastIndex
+//Filter
 
 
+title('TRANSFORM AN ARRAY');
+
+subTitle('Map method and sort method');
+console.log('map method and sort method');
+console.log('Sort method recive un numero positivo o negativo y trabaja igual de la misma manera esto permite resumir muchas cosas.');
+
+
+
+//Tasks
+title('Tasks');
+
+function camelize(str) {
+  return str.split('-').map((item, index) => index == 0 ? item : item[0].toUpperCase() + item.slice(1)).join('');
+}
+subTitle('Camelize function');
+let strPrueba = 'esta-es-una-prueba';
+let camelizePrueba = camelize(strPrueba);
+console.log(`${strPrueba} => ${camelizePrueba}`);
+
+subTitle('Filter range function');
+
+function filterRange(arr, a, b) {
+  return arr.filter(i => i >= a && i <= b);
+}
+
+let noFiltered = [5, 3, 8, 1];
+
+let filtered = filterRange(noFiltered, 1, 4);
+
+console.log(filtered);
+
+console.log(noFiltered);
 
